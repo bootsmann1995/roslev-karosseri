e9904b29bd8676d14f2c547e8ebee3beea784c0d
Anders Bootsmann <ab@lait.dk>
2024-03-09 10:35:56 +0100

    remove smooth

--- layouts/default.layout.ts
+++ layouts/default.layout.ts
@@ -8,28 +8,28 @@ export default defineNuxtComponent({
 		onMounted(() => {
 			gsap.registerPlugin(ScrollSmoother, ScrollTrigger);
 			// create the scrollSmoother before your scrollTriggers
-			ScrollSmoother.create({
-				smooth: 1, // how long (in seconds) it takes to "catch up" to the native scroll position
-				effects: true, // looks for data-speed and data-lag attributes on elements
-				smoothTouch: 0.1, // much shorter smoothing time on touch devices (default is NO smoothing on touch devices)
-			});
+			// ScrollSmoother.create({
+			// 	smooth: 1, // how long (in seconds) it takes to "catch up" to the native scroll position
+			// 	effects: true, // looks for data-speed and data-lag attributes on elements
+			// 	smoothTouch: 0.1, // much shorter smoothing time on touch devices (default is NO smoothing on touch devices)
+			// });
 
 			// window.addEventListener("resize", () => resetSmooth());
 		});
 
 		const resetSmooth = () => {
-			const smooth = ScrollSmoother.get();
-			setTimeout(() => {
-				smooth?.kill();
-			}, 100);
-			setTimeout(() => {
-				ScrollSmoother.create({
-					smooth: 1, // how long (in seconds) it takes to "catch up" to the native scroll position
-					effects: true, // looks for data-speed and data-lag attributes on elements
-					smoothTouch: 0.1, // much shorter smoothing time on touch devices (default is NO smoothing on touch devices)
-				});
-				ScrollTrigger.normalizeScroll(true);
-			}, 500);
+			// const smooth = ScrollSmoother.get();
+			// setTimeout(() => {
+			// 	smooth?.kill();
+			// }, 100);
+			// setTimeout(() => {
+			// 	ScrollSmoother.create({
+			// 		smooth: 1, // how long (in seconds) it takes to "catch up" to the native scroll position
+			// 		effects: true, // looks for data-speed and data-lag attributes on elements
+			// 		smoothTouch: 0.1, // much shorter smoothing time on touch devices (default is NO smoothing on touch devices)
+			// 	});
+			// 	ScrollTrigger.normalizeScroll(true);
+			// }, 500);
 		};
 		watch(
 			router.currentRoute,
