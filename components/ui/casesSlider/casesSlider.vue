42af9757da31975c7193652b12f338f3eefd1515
Anders Bootsmann <ab@lait.dk>
2024-07-18 10:57:17 +0200

    undo spacing change.

--- components/ui/casesSlider/casesSlider.vue
+++ components/ui/casesSlider/casesSlider.vue
@@ -1,7 +1,7 @@
 <template>
 	<div class="container-row ml-3 relative lg:ml-6 !mr-0 my-[100px] overflow-hidden">
 		<NuxtLink to="/cases/" class="absolute right-3 top-5 text-blue-500 font-bold group flex items-center">
-			Se alle cases
+			Se alle nyheder
 			<IconCSS
 				class="group-hover:translate-x-[5px] transition-transform ease-lait-ease duration-300"
 				name="tabler:arrow-right"
@@ -16,20 +16,25 @@
 				:breakpoints="{ 0: { slidesPerView: 1.5 }, 768: { slidesPerView: 4.5 } }"
 				class="w-full"
 			>
-				<SwiperSlide v-for="(item, index) in data.allCases" :key="index" class="group">
-					<NuxtLink :to="createLink(item)">
-						<div class="overflow-hidden">
-							<DatocmsImage
-								v-if="item.billede && item.billede.responsiveImage"
-								class="rounded-tl-md rounded-tr-md group-hover:scale-[1.05] transition-transform duration-300 ease-locomotive-ease"
-								:data="item.billede.responsiveImage"
-							/>
-						</div>
-
+				<SwiperSlide v-for="(item, index) in data.allCases" :key="index" class="group flex !h-auto">
+					<NuxtLink :to="createLink(item)" class="">
 						<div>
-							<h3 class="text-t3 text-[16px] text-night-black-500 mb-1 mt-3">{{ item.overskrift }}</h3>
-							<p class="text-[14px] text-gray-500 line-clamp-3">{{ item.teaser }}</p>
+							<div class="overflow-hidden">
+								<DatocmsImage
+									v-if="item.billede && item.billede.responsiveImage"
+									class="rounded-tl-md rounded-tr-md group-hover:scale-[1.05] transition-transform duration-300 ease-locomotive-ease"
+									:data="item.billede.responsiveImage"
+								/>
+							</div>
+
+							<div>
+								<h3 class="text-t3 text-[16px] text-night-black-500 mb-1 mt-3">
+									{{ item.overskrift }}
+								</h3>
+								<p class="text-[14px] text-gray-500 line-clamp-3">{{ item.teaser }}</p>
+							</div>
 						</div>
+						<p>læs mere</p>
 					</NuxtLink>
 				</SwiperSlide>
 			</Swiper>
